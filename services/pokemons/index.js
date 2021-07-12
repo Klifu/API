7df207b09eeba8eceb5f5d8d51ec750ab89f2dcf
Souvik <souvikde.ns@gmail.com>
2021-07-12 21:14:15 +0530

    feat: added pokemons logic

--- services/pokemons/index.js
+++ services/pokemons/index.js
@@ -1,8 +1,29 @@
 const app = require('express')();
+const Airtable = require('airtable');
+
+var base = new Airtable({ apiKey: process.env.API_KEY }).base(process.env.BASE);
+
+const list = async () => {
+	try {
+		let records = await base('Table 1').select({ view: 'Grid view' }).all();
+		let response = records.map(record => ({
+			id: record.get('id'),
+			name: record.get('name'),
+			type: record.get('type'),
+			sprite: record.get('sprite')[0].url
+		}))
+		return { response }
+	} catch (error) {
+		return { undefined, error };
+	}
+}
 
 
-app.get('/', (req, res) => {
-	return res.send("get pokemons list");
+app.get('/', async (req, res) => {
+	let { response, error } = await list();
+	if (error) return res.status(404).send(error);
+
+	return res.status(200).json(response);
 })
 
 module.exports = app;
