c57e88be9735e9e656f27f250bf24479b6c014af
Souvik <souvikde.ns@gmail.com>
2021-07-12 22:23:11 +0530

    chore: clean up 🧹

--- services/catch/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')();
-
-
-app.get('/', (req,res) => {
-	return res.send("Get random pokemon to catch");
-})
-
-module.exports = app;
--- services/pokemons/dev.js
+++
@@ -1,5 +0,0 @@
-const app = require('./index');
-
-app.listen(3001, () => {
-	console.log('Pokemons service running on server 3001');
-});
