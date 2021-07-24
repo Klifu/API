6050b78b199234b1901881f7f5e591abb344b0f8
Souvik <souvikde.ns@gmail.com>
2021-07-24 20:02:03 +0530

    feat: added swaggerUI for documentation

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const app = require('express')();
 const cors = require('cors');
-const {PokemonService} = require('./service');
+const { PokemonService } = require('./service');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 
 app.use(cors());
 
-app.get('/', (req,res) => {
-	return res.send("Welcome to klifu API and have fun!");
-});
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 
-app.get('/pokemons', async (req,res) => {
+app.get('/pokemons', async (req, res) => {
 	const pokemonService = new PokemonService();
-	const {pokemons, error} = await pokemonService.get();
-	if(error) return res.status(404).send(error);
+	const { pokemons, error } = await pokemonService.get();
+	if (error) return res.status(404).send(error);
 
 	return res.status(200).json(pokemons);
 })
