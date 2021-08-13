3f18271b4604883e192f2805e6da7deffdb1b2c7
0Verlord-41 <kinicooo1@gmail.com>
2021-08-14 00:38:43 +0530

    feat: Refactor

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const swaggerDoc = require('./swagger.json');
 
 app.use(cors());
 
-app.use(swaggerUI.serve );
+app.use(swaggerUI.serve);
 app.get('/', swaggerUI.setup(swaggerDoc));
 
-
-app.get('/pokemons', async (req, res) => {
+const getPokemons = async (req, res) => {
 	const pokemonService = new PokemonService();
 	const { pokemons, error } = await pokemonService.get();
 	if (error) return res.status(404).send(error);
 
 	return res.status(200).json(pokemons);
-})
+};
+
+app
+	.route('/pokemons')
+	.get(getPokemons);
 
 module.exports = app;
