9a08f3e210b9569dd489a2ce6ff35d08f1aa5c5c
0Verlord-41 <42108740+0Verlord-41@users.noreply.github.com>
2021-08-14 21:49:45 +0530

    feat: Refactor (#14)
    * feat: Refactor
    
    * feat: controller class
    
    * feat: re-controller class
    
    * feat: fixes
    
    Co-authored-by: Souvik <souvikde.ns@gmail.com>

---
+++ controller.js
@@ -0,0 +1,16 @@
+class PokemonController {
+    constructor(pokemonService) {
+        this.pokemonService = pokemonService;
+    }
+
+    async getAllPokemons(req, res) {
+        const { pokemons, error } = await this.pokemonService.get();
+        if (error) return res.status(404).send(error);
+
+        return res.status(200).json(pokemons);
+    }
+}
+
+module.exports = {
+    PokemonController
+}
--- index.js
+++ index.js
@@ -3,19 +3,15 @@ const cors = require('cors');
 const { PokemonService } = require('./service');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDoc = require('./swagger.json');
+const { PokemonController } = require('./controller');
+
+const pokemonController = new PokemonController(new PokemonService());
 
 app.use(cors());
 
-app.use(swaggerUI.serve );
+app.use(swaggerUI.serve);
 app.get('/', swaggerUI.setup(swaggerDoc));
 
-
-app.get('/pokemons', async (req, res) => {
-	const pokemonService = new PokemonService();
-	const { pokemons, error } = await pokemonService.get();
-	if (error) return res.status(404).send(error);
-
-	return res.status(200).json(pokemons);
-})
+app.get('/pokemons', async (req,res) => pokemonController.getAllPokemons(req,res));
 
 module.exports = app;
