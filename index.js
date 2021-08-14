0f06d0b37a4b8e571f412fcae849dcbec89ea780
0Verlord-41 <kinicooo1@gmail.com>
2021-08-14 20:47:07 +0530

    feat: controller class

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const cors = require('cors');
 const { PokemonService } = require('./service');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDoc = require('./swagger.json');
+const { PokemonController } = require('./controller');
+
+const pokemonService = new PokemonService();
+const allPokemons = new PokemonController(pokemonService);
 
 app.use(cors());
 
 app.use(swaggerUI.serve);
 app.get('/', swaggerUI.setup(swaggerDoc));
 
-const getPokemons = async (req, res) => {
-	const pokemonService = new PokemonService();
-	const { pokemons, error } = await pokemonService.get();
-	if (error) return res.status(404).send(error);
-
-	return res.status(200).json(pokemons);
-};
-
 app
 	.route('/pokemons')
-	.get(getPokemons);
+	.get(allPokemons.getPokemons());
 
 module.exports = app;
