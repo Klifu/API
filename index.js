a93891f247a818376208e8044a0f035f93a4893b
0Verlord-41 <kinicooo1@gmail.com>
2021-08-18 23:12:30 +0530

    feat: refactor

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const { PokemonService } = require('./service');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDoc = require('./swagger.json');
-const { PokemonController } = require('./controller/controller');
 
 const pokemonRouter = require('./routes/pokemonRoutes');
 
 app.use(cors());
 app.use(swaggerUI.serve);
+app.get('/', swaggerUI.setup(swaggerDoc));
+
 app.use('/', pokemonRouter);
 
 module.exports = app;
--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const swaggerUI = require('swagger-ui-express');
-const swaggerDoc = require('../swagger.json');
 const { PokemonService } = require('./../service');
 const { PokemonController } = require('./../controller/controller');
 
 const pokemonController = new PokemonController(new PokemonService());
 const router = express.Router();
-
-router
-    .route('/')
-    .get(swaggerUI.setup(swaggerDoc));
 
 router
     .route('/pokemons')
